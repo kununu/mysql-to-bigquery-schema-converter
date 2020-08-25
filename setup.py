1b071dd6d40e0fb2eaa5193c96e11a0be6b3e75d
Lorenzo Alfine <lorenzo.alfine@kununu.com>
2020-08-25 16:28:10 +0200

    Add setup.py

---
+++ setup.py
@@ -0,0 +1,22 @@
+import setuptools
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name="mysql-to-bigquery-schema-converter-lorrandalx",
+    version="0.0.1",
+    author="Lorenzo Alfine",
+    author_email="lorenzo.alfine@kununu.com",
+    description="MySql to BigQuery schema converter",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/kununulabs/mysql-to-bigquery-schema-converter",
+    packages=setuptools.find_packages(),
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+    python_requires='>=3.8',
+)
